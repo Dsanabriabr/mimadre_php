bfc3fa6a8a7ad741089869572b03c449101b43d4
Daniel Sanabria <sanabriabrasil@hotmail.com>
2024-11-05 16:28:16 -0300

    FIX Add Colum Conversations Table

--- database/migrations/2022_08_16_095504_update_converstions_table.php
+++ database/migrations/2022_08_16_095504_update_converstions_table.php
@@ -14,17 +14,12 @@ class UpdateConverstionsTable extends Migration
     public function up()
     {
         Schema::table('conversations', function (Blueprint $table) {
-            $table->renameColumn('user_id','sender_id');
-            $table->string('sender_type');
-            $table->foreignId('receiver_id');
-            $table->string('receiver_type');
-            $table->foreignId('last_message_id')->nullable();
-            $table->timestamp('last_message_time')->nullable();
-            $table->integer('unread_message_count')->default(0);
-            $table->dropColumn('message');
-            $table->dropColumn('reply');
-            $table->dropColumn('checked');
-            $table->dropColumn('image');
+            $table->string('sender_type')->nullable(false)->change();
+            $table->foreignId('receiver_id')->change();
+            $table->string('receiver_type')->nullable(false)->change();
+            $table->foreignId('last_message_id')->nullable()->change();
+            $table->dateTime('last_message_time')->nullable()->change();
+            $table->integer('unread_message_count')->default(0)->change();
         });
     }
 
@@ -36,16 +31,11 @@ class UpdateConverstionsTable extends Migration
     public function down()
     {
         Schema::table('conversations', function (Blueprint $table) {
-            $table->renameColumn('sender_id', 'user_id');
-            $table->string('message');
             $table->dropColumn('receiver_id');
             $table->dropColumn('receiver_type');
             $table->dropColumn('last_message_id')->nullable();
             $table->dropColumn('last_message_time')->nullable();
             $table->dropColumn('sender_type');
-            $table->string('reply');
-            $table->boolean('checked');
-            $table->string('image')->nullable();
         });
     }
 }
