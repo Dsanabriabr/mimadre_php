9ffd28b4f0926984ac43ef0bcbceb5a9099d18e9
Daniel Sanabria <sanabriabrasil@hotmail.com>
2024-11-05 16:27:15 -0300

    FIX Add Colum Coupons Table

--- database/migrations/2021_06_10_091547_add_column_to_coupons_table.php
+++ database/migrations/2021_06_10_091547_add_column_to_coupons_table.php
@@ -14,7 +14,6 @@ class AddColumnToCouponsTable extends Migration
     public function up()
     {
         Schema::table('coupons', function (Blueprint $table) {
-            $table->removeColumn('restaurant_id');
             $table->string('data')->nullable();
         });
     }
@@ -27,7 +26,6 @@ class AddColumnToCouponsTable extends Migration
     public function down()
     {
         Schema::table('coupons', function (Blueprint $table) {
-            $table->foreignId('restaurant_id');
             $table->removeColumn('data');
         });
     }
